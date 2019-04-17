926dfa1f98804bd912e4a3d470443d5788a74ad9
Adrian Lemes <ilegra.adrian@agiplan.com.br>
2019-04-17 20:51:54 -0300

    Add SSR with React

--- react-ssr-razzler/src/App.js
+++ react-ssr-razzler/src/App.js
@@ -1,11 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-const App = (props) => {
+const loadData = () => {
+  return axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+          return {
+              posts: response.data
+          };
+      });
+}
 
-  return (<div>
-   Name: {!props.firstName || !props.lastName ? 'Loading...' : `${props.firstName} ${props.lastName}`}
-  </div>) 
+const App = () => {
+  console.log("App normal");
+  const [postState, setPostState] = useState([]);
+
+  useEffect( () => {
+      loadData()
+        .then(result => {
+          setPostState(result.posts);
+        })
+
+  }, []);
+
+  return (
+    <div>
+      {postState.map(post => {
+        return <h1 key={post.id}> {post.title} </h1>;
+      })}
+    </div>
+  );
 };
 
 export default App;
